96539309e39e1b16046be99ca31c23c17268b22f
or-abdillh <hans.abdillh05@gmail.com>
2021-09-14 13:00:05 +0800

    Array dif

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-//  FIND UNIQUE ARRAY
-
-//INSTRUCTIONS : 
-//There is an array with some numbers. All numbers are equal except for one. Try to find it!
-// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
-// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
-
-function findUniq(arr) {
-  const unique = arr.filter( (x, i, a) => a.indexOf(x) == i)
-   for ( let x = 0; x < unique.length; x++ ) {
-      if ( arr.filter( item => item == unique[x]).length == 1 ) return (unique[x])
-   }
+// ARRAY DIFF
+// Your goal in this kata is to implement a difference function, 
+// which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+function arrayDiff(a, b) {
+  if ( a.length < 1 || b.length < 1 ) return a 
+  else {
+     let tmp = []
+     a.forEach( (arr, i) => {
+        if ( b.indexOf(arr) < 0 ) tmp.push(arr)
+     })
+     return tmp
+  }
 }
 
-const example = [3, 10, 3, 3, 3,3]
-console.log( findUniq(example) )
+console.log( arrayDiff([1,2,3], [1,2]) )
