953277492f195e792d9367f7e17df515473053c1
or-abdillh <hans.abdillh05@gmail.com>
2021-09-14 21:09:24 +0800

    Counting duplicates

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-// Jaden Casing strings
-// INSTRUCTIOND :
-// Your task is to convert strings to how they
-// would be written by Jaden Smith. The strings are actual quotes from Jaden Smith,
-// but they are not capitalized in the same way he originally typed them.
+//  Counting duplicates
+// INSTRUCTION :
+// Write a function that will return the count of distinct case-insensitive 
+// alphabetic characters and numeric digits that occur more than once in the input string. 
+// The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits
 
-String.prototype.toJadenCase = function () {
-  let str = this.toString().split(' ')
-  const jadenCase = []
-  str.forEach( item => {
-     let tmp = item.split('')
-     tmp[0] = tmp[0].toUpperCase()
-     jadenCase.push(tmp.join(''))
+function duplicateCount(text){
+  const uniques = text.toLowerCase().split('').filter( (x, i, a) => a.indexOf(x) === i) 
+  let count = 0
+  uniques.forEach( unique => {
+     if ( text.toLowerCase().split('').filter( item => item === unique ).length > 1) count++
   })
-  return jadenCase.join(' ')
+  return count
 }
 
-const str = "How can mirrors be real if our eyes aren't real"
-console.log(str.toJadenCase())
+const str = "aabBcde"
+console.log(duplicateCount(str))
+//console.log(str.indexOf('a'))
