afc44447c6efa6ad080b0ea8be8934e62eb19846
or-abdillh <hans.abdillh05@gmail.com>
2021-09-14 12:12:43 +0800

    Exes and Ohs

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-function alphabetPosition(text) {
-  const position = "abcdefghijklmnopqrstuvwxyz"
-  const postArr = []
-  text.toLowerCase().split('').forEach( item => {
-     if ( position.indexOf(item) >= 0 ) postArr.push(position.indexOf(item) + 1)
-  })
-  return postArr.join(' ')
+// EXES AND OHS
+// INSTRUCTIONS :
+// Check to see if a string has the same amount of 'x's and 'o's. 
+// The method must return a boolean and be case insensitive. 
+// The string can contain any char.
+
+//INPUT OUTPUT
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+   str = str.toLowerCase().split('')
+   const x = str.filter(item => item === 'x')
+   const o = str.filter(item => item === 'o')
+   
+   if ( x.length === o.length ) return true
+   else if ( x.length < 1 && o.length < 1 ) return true
+   else return false
 }
 
-const example = "The sunset sets at twelve o' clock"
-console.log( alphabetPosition(example))
+const example = 'Ooom'
+console.log(XO(example))
