4d960fd47b48f8d338811532fc681b0b1922daad
or-abdillh <hans.abdillh05@gmail.com>
2021-09-14 17:01:15 +0800

    Equal sides of an array

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-// FRIEND OR FOE ?
+// Equal sides of an array 
 // INSTRUCTIONS :
-// Make a program that filters a list of strings and returns a list with only your friends name in it.
-// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
-// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+// You are going to be given an array of integers. Your job is to take that array and find an index N 
+// where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. 
+// If there is no index that would make this happen, return -1.
 
-function friend(friends){
-  return friends.filter( friend => friend.length === 4)
+function findEvenIndex(numbers){
+   const size = numbers.length
+   let left
+   let right
+   for ( let x = 0; x < size; x++ ) {
+      let tmp = numbers[x]
+      if ( x == 0 ) {
+         left = numbers.slice(x + 1).reduce( (a, b) => a + b)
+         if ( tmp === left ) return x
+      } else {
+         left = numbers.slice(0, x).reduce( (a, b) => a + b)
+         if ( x < size - 1 ) right = numbers.slice(x + 1).reduce( (a, b) => a + b)
+         if ( left == right ) return x
+      }   
+   }
+   return -1
 }
 
-const example = ['ryan', 'karl', 'max']
-console.log(friend(example))
+const example = [20,10,30,10,10,15,35]
+console.log(findEvenIndex(example))
+//console.log(example.slice(1))
